6d373e807ce256fabb3983e417c4cd1f58deb3da
GyeongHwa <gyeonghwaa_@naver.com>
2024-06-09 19:31:10 +0900

    delete Deserializer

--- src/main/java/com/APSharing/entity/FcstItems.java
+++ src/main/java/com/APSharing/entity/FcstItems.java
@@ -1,19 +1,31 @@
-package com.APSharing;
+package com.APSharing.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.APSharing.entity.deserializer.FcstItemDeserializer;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Data
-@JsonDeserialize(using = FcstItemDeserializer.class)
+@AllArgsConstructor
 public class FcstItems {
     @JsonProperty("item")
     private List<FcstItem> fcstItems;
 
-    public FcstItems(List<FcstItem> fcstItems) {
-        this.fcstItems = fcstItems;
+    @JsonCreator
+    public FcstItems(@JsonProperty("response") JsonNode node) throws JsonProcessingException {
+        ObjectMapper objectMapper=new ObjectMapper();
+
+        JsonNode itemNode= node.findValue("item");
+        this.fcstItems= Arrays
+                .stream(objectMapper.treeToValue(itemNode, FcstItem[].class))
+                .toList();
     }
 }
