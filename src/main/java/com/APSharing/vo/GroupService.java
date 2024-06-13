70cb90287e81203f672cb1eff6fe02564c045a8d
GyeongHwa <gyeonghwaa_@naver.com>
2024-06-13 16:52:48 +0900

    AstroEvent 날짜 포맷 수정(2)

--- src/main/java/com/APSharing/vo/GroupService.java
+++ src/main/java/com/APSharing/vo/GroupService.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class GroupService {
     public List<Map<String, Object>> groupAstroEvents(List<AstroEventItem> items) {
         Map<String, List<AstroEventItem>> groupedItems = items.stream()
-                .collect(Collectors.groupingBy(AstroEventItem::getLocdate));
+                .collect(Collectors.groupingBy(AstroEventItem::getFormatLocdate));
 
         return groupedItems.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
